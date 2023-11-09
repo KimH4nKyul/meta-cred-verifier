b1a31c5981fc69b75c61e4f3d8a54f0f6e918e3d
KimH4nKyul <alskim93@kakao.com>
2023-11-09 13:45:53 +0900

    white_check_mark: VerifiedPresentation 테스트

---
+++ src/test/java/proj/toy/blockchain/metacredverifier/controller/VerifiedPresentationTest.java
@@ -0,0 +1,38 @@
+package proj.toy.blockchain.metacredverifier.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import proj.toy.blockchain.metacredverifier.controller.dto.response.VerifiedPresentation;
+import proj.toy.blockchain.metacredverifier.domain.VerifierDomain;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VerifiedPresentationTest {
+    @DisplayName("VerifierDomain으로 응답을 생성할 수 있음")
+    @Test
+    void can_create_response_from_verifier_domain() throws Exception {
+        // given
+        UUID id = UUID.randomUUID();
+
+        // when
+        VerifiedPresentation resposne = VerifiedPresentation.from(
+                VerifierDomain.builder()
+                        .id(id)
+                        .did("did:meta:1234")
+                        .hash("hash")
+                        .verified(true)
+                        .build()
+        );
+
+        // then
+        VerifiedPresentation finalResposne = resposne;
+        assertAll(() -> {
+            assertEquals(id, finalResposne.getId());
+            assertEquals("did:meta:1234", finalResposne.getDid());
+            assertEquals("hash", finalResposne.getHash());
+            assertTrue(resposne.isVerified());
+        });
+    }
+}
